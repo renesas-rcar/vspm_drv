56a8c6f626a8733425a5a4880e77612aa5737f91
Thinh Le <thinh.le.yn@renesas.com>
2026-05-04 17:32:36 +0700

    rcar_gen3: Remove do_exit from fw kthread for v6.1.166
    Signed-off-by: Thinh Le <thinh.le.yn@renesas.com>

--- vspm-module/files/vspm/drv/frame.c
+++ vspm-module/files/vspm/drv/frame.c
@@ -400,8 +400,6 @@ int fw_execute(unsigned short tid, struct fw_func_tbl *func_tbl)
 			wait_for_completion(&task_info->suspend);
 	}
 
-	/* Do exit kthread */
-	do_exit(FW_OK);
 	return FW_OK;
 }
 
